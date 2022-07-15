cc9eae7777fe63129e47f0d4e1ccdc88884d6ed4
Michael <micheaol80@gmail.com>
2022-07-15 08:03:56 +0100

    Add font family to the forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Password from './components/SetPassword/Password';
 import SuccessPage from './components/successPage/SuccessPage';
 import Register from './components/userRegistration/Register';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Register /> */}
-      {/* <Password /> */}
-      <SuccessPage />
-    </div>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="setpassword" element={<Password />} />
+        <Route path="success" element={<SuccessPage />} />
+      </Routes>
   );
 }
 
